d2afcdf88454f0e8626401bf716fcd868b08f8e7
lubert <misterzhu@gmail.com>
2015-09-06 12:04:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Requirements
+
+* Emacs 24.3 or greater
+
+## Installation
+
+To install, clone this repo to `~/.emacs.d`, i.e. ensure that the
+`init.el` contained in this repo ends up at `~/.emacs.d/init.el`:
+
+```
+git clone https://github.com/lubert/emacs.d.git ~/.emacs.d
---
+++ init.el
@@ -0,0 +1,195 @@
+;;; init.el --- emacs configuration settings
+
+;;; Commentary:
+;; Sane defaults and uses repositories as much as possible
+
+;;; Code:
+
+;; ---------------------
+;; -- Global Settings --
+;; ---------------------
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(load "defuns-config.el")
+(server-start)
+
+;; --------------
+;; -- Packages --
+;; --------------
+
+; Add repositories
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(package-initialize)
+
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+; List packages to install
+(ensure-package-installed
+ 'auto-complete
+ 'ac-js2
+ 'cider
+ 'find-file-in-repository
+ 'flycheck
+ 'flymake-go
+ 'go-mode
+ 'haskell-mode
+ 'icicles
+ 'js2-mode
+ 'less-css-mode
+ 'linum-off
+ 'magit
+ 'markdown-mode
+ 'nav
+ 'paredit
+ 'scss-mode
+ 'web-mode
+ 'zenburn-theme
+)
+
+
+;; -------------------
+;; -- Configuration --
+;; -------------------
+
+;; autocompile
+(add-hook 'after-save-hook (lambda ()
+			    (autocompile "~/.emacs.d/init.el")))
+
+;; autocomplete
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; guess-style
+(add-to-list 'load-path "~/.emacs.d/lisp/guess-style/")
+(autoload 'guess-style-set-variable "guess-style" nil t)
+(autoload 'guess-style-guess-variable "guess-style")
+(autoload 'guess-style-guess-all "guess-style" nil t)
+(autoload 'guess-style-guess-tab-width "guess-style" nil t)
+
+;; ido
+(require 'ido)
+(ido-mode t)
+
+;; js2-mode
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq js2-basic-offset 2)
+(setq js2-highlight-level 3)
+
+;; linum
+(require 'linum)
+(global-linum-mode 1)
+(add-hook 'term-mode-hook (lambda ()
+			    (linum-mode -1)))
+(add-hook 'grep-mode-hook (lambda ()
+			    (linum-mode -1)))
+(add-hook 'nav-mode-hook (lambda ()
+			   (linum-mode -1)))
+(add-hook 'eww-mode-hook (lambda ()
+			   (linum-mode -1)))
+
+(setq linum-format 'linum-highlight-current-line)
+
+;; nav
+(add-to-list 'load-path "~/.emacs.d/lisp/emacs-nav/")
+(require 'nav)
+
+;; paredit
+(add-hook 'js2-mode-hook 'my-paredit-nonlisp)
+(add-hook 'web-mode-hook 'my-paredit-nonlisp)
+
+;; python-mode
+(add-hook 'python-mode-hook 'guess-style-guess-tabs-mode)
+(add-hook 'python-mode-hook (lambda ()
+			      (when indent-tabs-mode
+				(guess-style-guess-tab-width))))
+
+;; recentf
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+
+;; uniquify
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
+;; web-mode
+(autoload 'web-mode "web-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+
+;; ---------------
+;; -- Variables --
+;; ---------------
+(menu-bar-mode 0) ; Disable menu bar
+(normal-erase-is-backspace-mode 1)
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(setq css-indent-offset 2)
+(setq column-number-mode t)
+(setq inhibit-startup-message t)
+(setq save-abbrevs nil)
+(setq show-trailing-whitespace t)
+(setq suggest-key-bindings t)
+(setq vc-follow-symlinks t)
+(setq require-final-newline t)
+(global-auto-revert-mode 1)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+;; ------------
+;; -- Macros --
+;; ------------
+(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
+(global-set-key "\M-=" 'align-equals)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(global-set-key "\C-c;" 'comment-or-uncomment-region)
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
+(global-set-key "\C-ct" '(lambda ()(interactive)(ansi-term "/bin/bash")))
+(global-set-key "\M-n" 'next5)
+(global-set-key "\M-p" 'prev5)
+(global-set-key "\M-o" 'other-window)
+(global-set-key "\M-i" 'back-window)
+(global-set-key "\C-z" 'zap-to-char)
+(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\M-d" 'delete-word)
+(global-set-key "\M-h" 'backward-delete-word)
+(global-set-key "\M-u" 'zap-to-char)
+(global-set-key (kbd "<f1>") 'nav-toggle)
+(global-set-key (kbd "<f2>") '(lambda () (interactive) (other-window -1)))
+(global-set-key (kbd "<f3>") 'switch-to-next-buffer)
+(global-set-key (kbd "<f4>") 'kmacro-start-macro)
+(global-set-key (kbd "<f5>") 'kmacro-end-or-call-macro)
+(global-set-key (kbd "<f6>") 'linum-mode)
+(global-set-key (kbd "<f7>") 'eval-defun)
+(global-set-key (kbd "\C-cg") 'goto-line)
+(global-set-key (kbd "\C-cs") '(lambda () (interactive) (split-window-vertically) (other-window 1) (shell)))
+(global-set-key (kbd "C-x f") 'find-file-in-repository)
+(global-set-key (kbd "C-x s") 'vc-git-grep2)
+(global-set-key ">" 'my-indent-region)
+(global-set-key "<" 'my-unindent-region)
+
+
+;; -----------
+;; -- Theme --
+;; -----------
+(load-theme 'zenburn t)
+(set-face-attribute 'linum nil :background "#303030")
+
+
+(provide 'init)
+
+;;; init.el ends here
