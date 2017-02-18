330ef285f481c01a07336524b5a8c2888bc32521
Lu Zhu <misterzhu@gmail.com>
2017-02-18 18:21:06 -0500

    Use js2-mode for jsx

--- init.el
+++ init.el
@@ -60,6 +60,11 @@
 (add-hook 'after-save-hook (lambda ()
 			    (autocompile "~/.emacs.d/init.el")))
 
+;; ac-js2
+(require 'ac-js2)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+(setq ac-js2-evaluate-calls t)
+
 ;; autocomplete
 (require 'auto-complete-config)
 (ac-config-default)
@@ -85,6 +90,7 @@
   (append flycheck-disabled-checkers
     '(javascript-jshint)))
 (flycheck-add-mode 'javascript-eslint 'web-mode)
+(flycheck-add-mode 'javascript-eslint 'js2-mode)
 (setq-default flycheck-disabled-checkers
   (append flycheck-disabled-checkers
     '(json-jsonlist)))
@@ -96,6 +102,8 @@
 ;; js2-mode
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
 (setq js2-basic-offset 2)
 (setq js2-highlight-level 3)
 
@@ -148,7 +156,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
