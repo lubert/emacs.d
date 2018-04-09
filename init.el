c2227dd908bb670b697a249dd45a199b639eee06
Lu Zhu <lu@plethora.com>
2018-04-09 17:53:26 +0000

    Add rjsx-mode

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
 ; 'haskell-mode
  'highlight-symbol
  'js2-mode
+ 'rjsx-mode
  'json-mode
  'less-css-mode
  'linum-off
@@ -92,6 +93,7 @@
     '(javascript-jshint)))
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 (flycheck-add-mode 'javascript-eslint 'js2-mode)
+(flycheck-add-mode 'javascript-eslint 'rjsx-mode)
 (setq-default flycheck-disabled-checkers
   (append flycheck-disabled-checkers
     '(json-jsonlist)))
@@ -103,10 +105,13 @@
 ;; js2-mode
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
 (setq js2-basic-offset 2)
 (setq js2-highlight-level 3)
+
+;; rjsx-mode
+(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("containers\\/.*\\.js\\'" . rjsx-mode))
 
 ;; linum
 (require 'linum)
