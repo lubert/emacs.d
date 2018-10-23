cdc72d7cd410ca7c3cfc7188dcb654e4dbab60e8
Lu Zhu <lu@plethora.com>
2018-10-23 22:33:20 +0000

    Add fci-mode fix for ac

--- init.el
+++ init.el
@@ -91,6 +91,17 @@
            (not (eq major-mode 'dired-mode)))
           (fci-mode 1))))
   (global-fci-mode 1)
+(defvar sanityinc/fci-mode-suppressed nil)
+(defadvice popup-create (before suppress-fci-mode activate)
+  "Suspend fci-mode while popups are visible"
+  (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
+  (when fci-mode
+    (turn-off-fci-mode)))
+(defadvice popup-delete (after restore-fci-mode activate)
+  "Restore fci-mode when all popups have closed"
+  (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
+    (setq sanityinc/fci-mode-suppressed nil)
+    (turn-on-fci-mode)))
 
 ;; flycheck
 (require 'flycheck)
