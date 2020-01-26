97652055722facfe20eb4bd38ef4df6537c6d8f0
Lu Zhu <misterzhu@gmail.com>
2020-01-26 23:16:26 +0000

    Add dumb jump

--- init.el
+++ init.el
@@ -195,6 +195,15 @@
   :init
   (add-to-list 'company-backends 'company-irony-c-headers))
 
+(use-package dumb-jump
+  :ensure t
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g b" . dumb-jump-back)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window)))
+
 (use-package elpy
   :ensure t
   :init
