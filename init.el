47783d08aea575d039aac74cfa2e6af1c55c8b01
Lu Zhu <lu.zhu@instacart.com>
2020-05-19 09:13:30 -0700

    Add back vc grep and turn on ivy

--- init.el
+++ init.el
@@ -50,6 +50,40 @@
     (propertize (format (concat "%" linum-border-width "d ") line-number)
                 'face face)))
 
+(defun vc-git-grep2 (regexp dir)
+  (interactive
+   (progn
+     (grep-compute-defaults)
+     (cond
+      ((equal current-prefix-arg '(16))
+       (list (read-from-minibuffer "Run: " "git grep" nil nil 'grep-history)
+	     nil))
+      (t (let* ((regexp (grep-read-regexp))
+		(dir (read-directory-name "In directory: " nil default-directory t)))
+	   (list regexp dir))))))
+  (require 'grep)
+  (when (and (stringp regexp) (> (length regexp) 0))
+    (let ((command regexp))
+      (if (> 4 5)
+	  (if (string= command "git grep")
+	      (setq command nil))
+	(setq dir (file-name-as-directory (expand-file-name dir)))
+	(setq command
+	      (grep-expand-template "git grep -n -i -e <R>" regexp))
+	(when command
+	  (if (equal current-prefix-arg '(4))
+	      (setq command
+		    (read-from-minibuffer "Confirm: " command nil nil 'grep-history))
+	    (add-to-history 'grep-history command))))
+      (when command
+	(let ((default-directory dir)
+	      (compilation-environment '("PAGER=")))
+	  ;; Setting process-setup-function makes exit-message-function work
+	  ;; even when async processes aren't supported.
+	  (compilation-start command 'grep-mode))
+	(if (eq next-error-last-buffer (current-buffer))
+            (setq default-directory dir))))))
+
 ;; ------------
 ;; -- Macros --
 ;; ------------
@@ -59,6 +93,7 @@
 (global-set-key "\M-o" 'other-window)
 (global-set-key "\M-d" 'subword-kill)
 (global-set-key "\M-h" 'subword-backward-kill)
+(global-set-key "\C-xs" 'vc-git-grep2)
 (autoload 'zap-up-to-char "misc" 'interactive)
 (global-set-key "\M-z" 'zap-up-to-char)
 
@@ -150,6 +185,7 @@
   :init
   (setq ivy-re-builders-alist
         '((t . ivy--regex-plus)))
+  (ivy-mode 1)
   :ensure t)
 
 (use-package linum
