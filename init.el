7293de31b75b67184de0de86a38a229c0ba9b3ca
Lu Zhu <lu.zhu@instacart.com>
2020-06-05 13:03:40 -0700

    Add back vc-git-grep2

--- init.el
+++ init.el
@@ -84,14 +84,39 @@
 ;; ------------
 ;; -- Macros --
 ;; ------------
-
-(define-key key-translation-map [?\C-h] [?\C-?])
-(global-set-key "\M-o" 'other-window)
-(global-set-key "\M-d" 'subword-kill)
-(global-set-key "\M-h" 'subword-backward-kill)
-(global-set-key "\M-z" 'zap-up-to-char)
-(global-set-key "\C-xs" 'vc-git-grep)
-(autoload 'zap-up-to-char "misc" 'interactive)
+(defun vc-git-grep2 (regexp dir)
+  (interactive
+   (progn
+     (grep-compute-defaults)
+     (cond
+      ((equal current-prefix-arg '(16))
+       (list (read-from-minibuffer "Run: " "git grep" nil nil 'grep-history)
+    nil))
+      (t (let* ((regexp (grep-read-regexp))
+    (dir (read-directory-name "In directory: " nil default-directory t)))
+     (list regexp dir))))))
+  (require 'grep)
+  (when (and (stringp regexp) (> (length regexp) 0))
+    (let ((command regexp))
+      (if (> 4 5)
+    (if (string= command "git grep")
+     (setq command nil))
+  (setq dir (file-name-as-directory (expand-file-name dir)))
+  (setq command
+     (grep-expand-template "git grep -n -i -e <R>" regexp))
+  (when command
+    (if (equal current-prefix-arg '(4))
+     (setq command
+     (read-from-minibuffer "Confirm: " command nil nil 'grep-history))
+   (add-to-history 'grep-history command))))
+      (when command
+  (let ((default-directory dir)
+     (compilation-environment '("PAGER=")))
+    ;; Setting process-setup-function makes exit-message-function work
+    ;; even when async processes aren't supported.
+    (compilation-start command 'grep-mode))
+  (if (eq next-error-last-buffer (current-buffer))
+   (setq default-directory dir))))))
 
 (defun doom-unquote (exp)
   "Return EXP unquoted."
@@ -117,6 +142,14 @@ list is returned as-is."
                if (eq (car-safe hook) 'quote)
                collect (cadr hook)
                else collect (intern (format "%s-hook" (symbol-name hook)))))))
+
+(define-key key-translation-map [?\C-h] [?\C-?])
+(global-set-key "\M-o" 'other-window)
+(global-set-key "\M-d" 'subword-kill)
+(global-set-key "\M-h" 'subword-backward-kill)
+(global-set-key "\M-z" 'zap-up-to-char)
+(global-set-key "\C-xs" 'vc-git-grep2)
+(autoload 'zap-up-to-char "misc" 'interactive)
 
 ;; --------------
 ;; -- Packages --
@@ -356,6 +389,13 @@ list is returned as-is."
 
 (use-package terraform-mode
   :mode "\\.tf\\'"
+  :ensure)
+
+(use-package tide
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save))
   :ensure)
 
 (use-package uniquify
