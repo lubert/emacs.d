846bcbcf73bae14e22050d15fcc9feb075e409e7
Lu Zhu <lu.zhu@instacart.com>
2020-06-30 11:00:17 -0700

    Remove format before save

--- init.el
+++ init.el
@@ -395,8 +395,7 @@ list is returned as-is."
 (use-package tide
   :after (typescript-mode company flycheck)
   :hook ((typescript-mode . tide-setup)
-         (typescript-mode . tide-hl-identifier-mode)
-         (before-save . tide-format-before-save))
+         (typescript-mode . tide-hl-identifier-mode))
   :config (flycheck-add-mode 'typescript-tslint 'web-mode)
   :ensure)
 
