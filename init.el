8155b09b9945ac97880ed45db0ef62b39fe7f257
Lu Zhu <lu.zhu@instacart.com>
2021-08-22 23:46:08 -0700

    Add back yasnippet

--- init.el
+++ init.el
@@ -387,6 +387,21 @@
   :config (ws-butler-global-mode 1)
   :ensure)
 
+(use-package yasnippet-snippets
+  :after yasnippet
+  :config (yasnippet-snippets-initialize)
+  :ensure)
+
+(use-package yasnippet
+  :commands yas--get-snippet-tables
+  :config (yas-global-mode)
+  :hook (yas-minor-mode . my/disable-yas-if-no-snippets)
+  :preface
+  (defun my/disable-yas-if-no-snippets ()
+    (when (and yas-minor-mode (null (yas--get-snippet-tables)))
+      (yas-minor-mode -1)))
+  :ensure)
+
 (use-package zenburn-theme
   :config (load-theme 'zenburn t)
   :ensure)
